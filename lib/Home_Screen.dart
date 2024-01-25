5ab7dce8ce124371aafdc96f695e5003914242ab
dimaderevciuc <dimaderevciuc@gmail.com>
2024-01-25 03:05:31 +0200

    final commit

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -10,7 +11,7 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   String userInput = "";
   String result = "0";
-  
+
   List<String> buttonList = [
     'AC',
     '(',
@@ -69,32 +70,41 @@ class _CalculatorState extends State<Calculator> {
               ],
             ),
           ),
-          Divider( color: Colors.white,),
-          Expanded(child: Container(
-            padding: EdgeInsets.all(10),
-            child: GridView.builder(
-              itemCount: buttonList.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              crossAxisSpacing: 12,
-              mainAxisSpacing: 12,
-            ), itemBuilder: (BuildContext context, int index){
-              return CustomButton(buttonList[index]);
-            },
+          Divider(
+            color: Colors.white,
           ),
-          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: GridView.builder(
+                itemCount: buttonList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomButton(buttonList[index]);
+                },
+              ),
+            ),
           ),
         ],
       ),
     );
   }
-  Widget CustomButton(String text){
+
+  Widget CustomButton(String text) {
     return InkWell(
       splashColor: Colors.black,
-      onTap: (){},
+      onTap: () {
+        setState(() {
+          handleButtons(text);
+        });
+      },
       child: Ink(
         decoration: BoxDecoration(
-          color: getBgColor(text),  
+          color: getBgColor(text),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
@@ -118,6 +128,63 @@ class _CalculatorState extends State<Calculator> {
       ),
     );
   }
-  getColor(S)
-  // no changes
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "C" ||
+        text == "(" ||
+        text == ")") {
+      return Colors.green[400];
+    }
+    return Colors.white;
+  }
+
+  getBgColor(String text) {
+    if (text == "AC") {
+      return Colors.green[400];
+    }
+    return Colors.black;
+  }
+
+  handleButtons(String text) {
+    if (text == "AC") {
+      userInput = "";
+      result = "0";
+      return;
+    }
+    if (text == "C") {
+      if (userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length - 1);
+        return;
+      } else {
+        return null;
+      }
+    }
+    if (text == "=") {
+      result = calculate();
+      userInput = result;
+      if (userInput.endsWith(".0")) {
+        userInput = userInput.replaceAll(".0", "");
+      }
+
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+        return;
+      }
+    }
+    userInput = userInput + text;
+  }
+
+  String calculate() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Error";
+    }
+  }
 }
